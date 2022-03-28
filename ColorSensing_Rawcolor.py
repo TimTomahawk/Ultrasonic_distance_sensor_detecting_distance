3b20f1cb66a34ad37a9080f8d502c8edb3304aeb
TimTomahawk <timguan0806@gmail.com>
2022-03-28 02:57:31 -0400

    Add color sensor 2

--- ColorSensing_Rawcolor.py
+++ ColorSensing_Rawcolor.py
@@ -3,17 +3,24 @@ import time
 
 s2 = 8
 s3 = 7
-signal = 12
+signal_1 = 12
 NUM_CYCLES = 10
+
+s4 = 17
+s5 = 27
+signal_2 = 22
 
 def setup():  ## def setup
   GPIO.setmode(GPIO.BCM)
-  GPIO.setup(signal,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  GPIO.setup(signal_1,GPIO.IN, pull_up_down=GPIO.PUD_UP)
   GPIO.setup(s2,GPIO.OUT)
   GPIO.setup(s3,GPIO.OUT)
+  GPIO.setup(signal_2,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  GPIO.setup(s4,GPIO.OUT)
+  GPIO.setup(s5,GPIO.OUT)
   print("\n")
 
-def loop():
+def loop_1():
   temp = 1
   while(1):
 
@@ -26,8 +33,8 @@ def loop():
     for impulse_count in range(NUM_CYCLES):
       GPIO.wait_for_edge(signal, GPIO.FALLING)
     duration = time.time() - start      #seconds to run for loop
-    white  = NUM_CYCLES / duration   #in Hz
-    print("white value - ",white)
+    white_1  = NUM_CYCLES / duration   #in Hz
+    print("white value_1 - ",white_1)
 
     GPIO.output(s2,GPIO.LOW)
     GPIO.output(s3,GPIO.HIGH)
@@ -38,8 +45,38 @@ def loop():
     for impulse_count in range(NUM_CYCLES):
       GPIO.wait_for_edge(signal, GPIO.FALLING)
     duration = time.time() - start
-    blue = NUM_CYCLES / duration
-    print("blue value - ",blue)
+    blue_1 = NUM_CYCLES / duration
+    print("blue value_1 - ",blue_1)
+
+    time.sleep(0.5)
+
+def loop_2():
+  temp = 1
+  while(1):
+
+    GPIO.output(s4,GPIO.HIGH)
+    GPIO.output(s5,GPIO.LOW)
+
+    time.sleep(0.3)
+
+    start = time.time()
+    for impulse_count in range(NUM_CYCLES):
+      GPIO.wait_for_edge(signal, GPIO.FALLING)
+    duration = time.time() - start      #seconds to run for loop
+    white_2  = NUM_CYCLES / duration   #in Hz
+    print("white value_2 - ",white_2)
+
+    GPIO.output(s4,GPIO.LOW)
+    GPIO.output(s5,GPIO.HIGH)
+
+    time.sleep(0.3)
+
+    start = time.time()
+    for impulse_count in range(NUM_CYCLES):
+      GPIO.wait_for_edge(signal, GPIO.FALLING)
+    duration = time.time() - start
+    blue_2 = NUM_CYCLES / duration
+    print("blue value_2 - ",blue_2)
 
     time.sleep(0.5)
 
@@ -52,7 +89,8 @@ if __name__=='__main__':
     setup()
 
     try:
-        loop()
+        loop1()
+        loop2()
 
     except KeyboardInterrupt:
         endprogram()
