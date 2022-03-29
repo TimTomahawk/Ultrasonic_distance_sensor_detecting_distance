e658b1d102913d0ed96f6ef1c1117715f6b7b66c
TimTomahawk <timguan0806@gmail.com>
2022-03-28 20:46:42 -0400

    update comment

--- Rawcolor_sensing_v2.py
+++ Rawcolor_sensing_v2.py
@@ -1,18 +1,18 @@
-import RPi.GPIO as GPIO
-import time
+import RPi.GPIO as GPIO   ## import raspberry pi GPIO control library
+import time        ## import time library
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)  ## set up setmode
 
-s2 = 8
+s2 = 8                  ## line 6-13 are the pins
 s3 = 7
 signal_1 = 12
-NUM_CYCLES = 10
+NUM_CYCLES = 10         ## num cycle in for loop
 
 s4 = 17
 s5 = 27
 signal_2 = 22
 
-GPIO.setup(signal_1,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(signal_1,GPIO.IN, pull_up_down=GPIO.PUD_UP)         ##setup pin function
 GPIO.setup(s2,GPIO.OUT)
 GPIO.setup(s3,GPIO.OUT)
 
@@ -20,9 +20,9 @@ GPIO.setup(signal_2,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(s4,GPIO.OUT)
 GPIO.setup(s5,GPIO.OUT)
 
-def CS_1_white():
+def CS_1_white():                ## function for first color sensor to detect clear(white) color
 
-    GPIO.output(s2,GPIO.HIGH)
+    GPIO.output(s2,GPIO.HIGH)         # s2 setup as high and s3 setup as low detects the frequency for white color
     GPIO.output(s3,GPIO.LOW)
 
     time.sleep(0.3)
@@ -34,9 +34,9 @@ def CS_1_white():
     white_1  = NUM_CYCLES / duration   #in Hz
     return white_1
 
-def CS_1_blue():
+def CS_1_blue():  ## function for first color sensor to detect blue color
 
-    GPIO.output(s2,GPIO.LOW)
+    GPIO.output(s2,GPIO.LOW)      ## s2 setup as low, s3 setup as high detects the frequency for blue color
     GPIO.output(s3,GPIO.HIGH)
 
     time.sleep(0.3)
@@ -48,7 +48,7 @@ def CS_1_blue():
     blue_1 = NUM_CYCLES / duration
     return blue_1
 
-def CS_2_white():
+def CS_2_white():   ## function for second color sensor to detect white color
 
     GPIO.output(s4,GPIO.HIGH)
     GPIO.output(s5,GPIO.LOW)
@@ -62,7 +62,7 @@ def CS_2_white():
     white_2  = NUM_CYCLES / duration   #in Hz
     return white_2
 
-def CS_2_blue():
+def CS_2_blue():  ## function for second color sensor to detect blue color
 
     GPIO.output(s4,GPIO.LOW)
     GPIO.output(s5,GPIO.HIGH)
@@ -79,7 +79,7 @@ def CS_2_blue():
     time.sleep(0.5)
 
 
-def endprogram():
+def endprogram():    ## function for press crtl+c to stop 
     GPIO.cleanup()
 
 if __name__=='__main__':
