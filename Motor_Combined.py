bf9b10d93a368e67c38c2ba2f4add6fa31fa6467
Tim G <timguan0806@gmail.com>
2022-04-05 15:37:09 -0400

    upload color frequencies for testing

--- Motor_Combined.py
+++ Motor_Combined.py
@@ -33,10 +33,10 @@ in8 = 19
 tDistance = 3  ## tolerance distance
 sleepT= 30   ## turning from x to y time used
 
-white_white = 10000   ## white frequency while detecting white
-white_blue = 3500    ## blue freqneucy value while detecting white
-blue_white = 2000    ## white frequency value while detecting blue
-blue_blue = 1300    ##blue frequency value while detecting blue
+white_white = 6500   ## white frequency while detecting white
+white_blue = 3000   ## blue freqneucy value while detecting white
+blue_white = 5000   ## white frequency value while detecting blue
+blue_blue = 2800    ##blue frequency value while detecting blue
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
